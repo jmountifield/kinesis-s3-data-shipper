3e7a3872036aa7721d69e0eeb95cdbc1949b502f
James Mountifield <james.mountifield@gmail.com>
2020-09-17 13:42:54 +0100

    Working with compression, cli options, etc

--- kinesis-to-humio.py
+++ kinesis-to-humio.py
@@ -1,4 +1,3 @@
-import boto3
 import sys
 import datetime
 import tempfile
@@ -6,59 +5,70 @@ import gzip
 import mmap
 import os
 import json
-import urllib3
-import urllib.parse
 import shutil
+import urllib.parse
+import urllib3
+import boto3
 
 
 # Bytes string that identifies the start of the array
-arrary_start = b"{\"messageType\":\"DATA_MESSAGE\""
+ARRAY_START = b"{\"messageType\":\"DATA_MESSAGE\""
 
 
-def humioUrl(args):
+
+def humio_url(args):
+    """Return the URL to Humio's structured ingest API"""
     return urllib.parse.urljoin(args['humio-host'], '/api/v1/ingest/humio-structured')
 
-def humioHeaders(args):
-    # Humio structured api endpoint
+
+
+def humio_headers(args):
+    """Humio structured api endpoint"""
     return {'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + args['humio-token']}
 
 
-# A cheap little log printer
-def basicLog(message, level='INFO'):
+
+def log(message, level='INFO'):
+    """A cheap little log line printer"""
     print("%s [%s] %s"% (datetime.datetime.now(), level, message))
 
 
-def isCompressed(file):
+
+def is_compressed(filename):
     """Detects if a file at a specific path is gzip compressed."""
     try:
-        gzip.GzipFile(filename=file).peek(64)
+        gzip.GzipFile(filename=filename).peek(64)
         return True
-    except OSError as e:
+    except OSError:
         return False
 
 
-def parseAndSend(args, file, http, client):
+
+def parse_and_send(args, file, http, client):
+    """For the file given we will extract the relevent events, parse them, and
+    send them to Humio."""
 
     # We're going to process each file in a dedicated temp directory
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    with tempfile.TemporaryDirectory(dir=args['tmpdir']) as tmpdirname:
+
         # Get the filename from the file path
-        filePath = os.path.join(tmpdirname, os.path.basename(file))
+        file_path = os.path.join(tmpdirname, os.path.basename(file))
 
         # Download the source file from S3
-        client.download_file(args['bucket'], file, filePath)
+        client.download_file(args['bucket'], file, file_path)
 
         # Now we may need to decompress the file, and potentially even TWICE, eurgh
-        while isCompressed(filePath):
+        while is_compressed(file_path):
             # Decompress the file in-place
-            with gzip.open(filePath, 'rb') as f_in, open(filePath + ".working", 'wb') as f_out:
+            with gzip.open(file_path, 'rb') as f_in, open(file_path + ".working", 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
             # Move the new file over the top of the original
-            shutil.move(filePath + ".working", filePath)
+            shutil.move(file_path + ".working", file_path)
 
         # Open the new uncompressed version of the download and mmap it (it could be big!)
-        with open(filePath) as cloudwatchEvents:
-            mm = mmap.mmap(cloudwatchEvents.fileno(), 0, prot=mmap.PROT_READ)
+        with open(file_path) as cw_events_fh:
+            cw_events_mm = mmap.mmap(cw_events_fh.fileno(), 0, prot=mmap.PROT_READ)
 
             # Start searching at the beginning of the file
             start_search = 0
@@ -66,15 +76,16 @@ def parseAndSend(args, file, http, client):
 
             # Search through the file looking for the message blocks
             while start_search != -1:
-                start_search = mm.find(arrary_start, start_search)
+                start_search = cw_events_mm.find(ARRAY_START, start_search)
                 # If we found another message block then record it, and search again
                 if start_search != -1:
                     message_positions.append(start_search)
                     start_search += 1
 
-            basicLog("Found %d message blocks in %s"% (len(message_positions), file), level="INFO")
-
-            # sys.exit()
+            if len(message_positions) == 0:
+                log("Found %d message blocks in %s"% (len(message_positions), file), level="WARN")
+            else:
+                log("Found %d message blocks in %s"% (len(message_positions), file), level="INFO")
 
             # At this point we start processing the events and sending them to Humio
             # in batches up to humio-batch events
@@ -87,17 +98,17 @@ def parseAndSend(args, file, http, client):
                     length = -1
 
                 # Load the message blocks and parse as JSON
-                mm.seek(start_pos)
-                json_data = json.loads(mm.read(length))
+                cw_events_mm.seek(start_pos)
+                json_data = json.loads(cw_events_mm.read(length))
 
-                if args['debug']: basicLog("Found %d events to process from pos [%d] length [%d] in %s"% (len(json_data['logEvents']), start_pos, length, file), level="DEBUG")
+                if args['debug']: log("Found %d events to process from pos [%d] length [%d] in %s"% (len(json_data['logEvents']), start_pos, length, file), level="DEBUG")
 
                 # The bare payload for Humio structured API request
                 payload = [ { "tags": {}, "events": [] } ]
                 payload[0]['tags']['logStreamPrefix'] =  '/'.join(json_data['logStream'].split('/')[0:2])
                 payload[0]['tags']['logGroup'] = json_data['logGroup']
 
-                hadError = False
+                had_error = False
 
                 # Process each event to build the payload
                 events_to_process = len(json_data['logEvents'])
@@ -109,12 +120,12 @@ def parseAndSend(args, file, http, client):
                     if (len(payload[0]['events']) == args['humio_batch']) or (i == (events_to_process - 1)):
                         ##### SEND HERE #####
                         encoded_data = json.dumps(payload).encode('utf-8')
-                        r = http.request('POST', humioUrl(args), body=encoded_data, headers=humioHeaders(args))
+                        r = http.request('POST', humio_url(args), body=encoded_data, headers=humio_headers(args))
                         if r.status == 200:
-                            if args['debug']: basicLog("Sent %d events to Humio successfully"% len(payload[0]['events']), level="DEBUG")
+                            if args['debug']: log("Sent %d events to Humio successfully"% len(payload[0]['events']), level="DEBUG")
                         else:
-                            basicLog("Failed to send %d events to Humio, status=%d"% (len(payload[0]['events']), r.status), level="ERROR")
-                            hadError = True
+                            log("Failed to send %d events to Humio, status=%d"% (len(payload[0]['events']), r.status), level="ERROR")
+                            had_error = True
 
                         # Reset the payload
                         payload = [ { "tags": {}, "events": [] } ]
@@ -123,45 +134,53 @@ def parseAndSend(args, file, http, client):
                         payload[0]['tags']['logGroup'] = json_data['logGroup']
 
 
-                if hadError:
-                    basicLog("Error sending some events from file %s"% filePath, level="ERROR")
+                if had_error:
+                    log("Error sending some events from file %s"% file_path, level="ERROR")
                 else:
-                    basicLog("Sent %d events from file %s"% (len(json_data['logEvents']), file), level="INFO")
+                    log("Sent %d events from file %s"% (len(json_data['logEvents']), file), level="INFO")
 
 
 
-def findFiles(args, client):
-    # Get a list of all the objects to process
+def find_files(args, client):
+    """ Get a list of all the objects to process"""
     objects = []
-    continuationToken = 'UNSET'
-    while continuationToken:
-        if continuationToken == 'UNSET':
-            bucketList = client.list_objects_v2(Bucket=args['bucket'],Prefix=args['prefix'])
+    continuation_token = 'UNSET'
+    while continuation_token:
+        if continuation_token == 'UNSET':
+            object_list = client.list_objects_v2(Bucket=args['bucket'],Prefix=args['prefix'])
         else:
-            bucketList = client.list_objects_v2(Bucket=args['bucket'], Prefix=args['prefix'], ContinuationToken=continuationToken)
+            object_list = client.list_objects_v2(Bucket=args['bucket'], Prefix=args['prefix'], ContinuationToken=continuation_token)
 
-        if args['debug']: basicLog("Found %d items from bucket list_objects_v2(), includes dirs."% bucketList['KeyCount'], level="DEBUG")
+        if args['debug']: log("Found %d items from bucket list_objects_v2(), includes dirs."% object_list['KeyCount'], level="DEBUG")
 
         # This means we have no more keys, or none found
-        if bucketList['KeyCount'] > 0:
-            for item in bucketList['Contents']:
+        if object_list['KeyCount'] > 0:
+            for item in object_list['Contents']:
                 if not item['Key'].endswith('/'): # ignore directories
                     objects.append(item['Key'])
 
         # And here we check to see if there's more results to recover
-        if bucketList['IsTruncated']:
-            continuationToken = bucketList['NextContinuationToken']
+        if object_list['IsTruncated']:
+            continuation_token = object_list['NextContinuationToken']
         else:
-            continuationToken = False
+            continuation_token = False
 
     # What did we get?
-    basicLog("Found %d items"% len(objects))
+    log("Found %d items"% len(objects))
 
     return objects
 
 
 
-def prettyPrintArgs(args):
+def is_suitable_tempdir(path):
+    if os.path.isdir(path) and os.access(path, os.W_OK):
+        return path
+    msg = "%s is not a usable temp dir" % path
+    raise argparse.ArgumentTypeError(msg)
+
+
+
+def pp_args(args):
     print("Running with the following arguments:")
     print()
     for arg in args:
@@ -173,8 +192,9 @@ def prettyPrintArgs(args):
     print()
 
 
+
 if __name__ == "__main__":
-    
+
     # We only need to do the argparse if we're running interactivley
     import argparse
 
@@ -193,21 +213,22 @@ if __name__ == "__main__":
 
     # Are we going to do the debug?
     parser.add_argument('--debug',           action='store_true', help='We do the debug?')
+    parser.add_argument('--tmpdir',          type=is_suitable_tempdir, action='store', default="/tmp", help='The temp directory where the work will be done')
 
     # Build the argument list
     args = vars(parser.parse_args())
 
     # Echo to stdout so we can see the args in use if debug
-    if args['debug']: prettyPrintArgs(args)
+    if args['debug']: pp_args(args)
 
     # Initialise the S3 client
     client = boto3.client('s3')
 
     # Find the files to download and process
-    filesToProcess = findFiles(args, client)
+    filesToProcess = find_files(args, client)
 
     if not filesToProcess:
-        basicLog("No Objects Found. If you think there should be then check yo' prefix.")
+        log("No Objects Found. If you think there should be then check yo' prefix.")
         sys.exit()
 
     # We're going to start sending some events so setup the http connection pool
@@ -215,9 +236,6 @@ if __name__ == "__main__":
 
     # Process each file that we want to send to Humio
     for file in sorted(filesToProcess):
-        parseAndSend(args, file, http, client)
+        parse_and_send(args, file, http, client)
 
-
-    # print(args)
     sys.exit()
-

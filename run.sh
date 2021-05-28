9ff827bf5a5c94121d59664ef97327b76eaa37eb
James Mountifield <james.mountifield@gmail.com>
2021-05-28 16:46:10 +0100

    Add example run file showing env vars to use

---
+++ run.sh
@@ -0,0 +1,13 @@
+export AWS_ACCESS_KEY_ID=XXXXXXXXXXXX
+export AWS_SECRET_ACCESS_KEY=YYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+export AWS_DEFAULT_REGION=eu-west-1
+
+# Note: client region above must match the sqs queue region below
+
+python3 cloudtrail-to-humio.py \
+	your-s3-bucket-name \
+	https://yuo.humio.server/ INGEST_TOKEN \
+	--debug \
+	--sqs-queue "https://sqs.eu-west-1.amazonaws.com/9876547876/cloudtrail-sqs-queue-name" \
+	--continuous \
+	--sleep 0
